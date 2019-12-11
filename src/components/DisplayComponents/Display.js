cacf896d421827a9ea669947aaf9f795e082a987
Gus Baden <gusbaden96@gamil.com>
2019-12-10 16:09:36 -0800

    finished mvp

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,5 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
+import { tsPropertySignature } from "@babel/types";
+import SpecialButton from "../ButtonComponents/SpecialButtons/SpecialButton";
 
-const Display = () => {
-  return <div className="display">{/* Display any props data here */}</div>;
+const Display = (props) => {
+  const[display, setDisplay] = useState(0);
+  return <div className="display">{display}</div>;
 };
+
+export default Display;
